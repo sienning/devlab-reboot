49720a8c9903c97e7f16dec3f7554f7f8acd5d6d
Cabot <victor.cabot@edu.devinci.fr>
2020-01-10 09:42:34 +0100

    UPTADE onclick, logo, menu

--- js/header.js
+++ js/header.js
@@ -15,6 +15,10 @@ $(document).ready(function() {
     return false;
     
   });
+
+  $('.test').click(function() {
+  	  $(this).toggleClass('bg');
+  })
   
   // On scroll, remove class on active element and add it to the new one
   
@@ -26,5 +30,26 @@ $(document).ready(function() {
      $('header nav a.link-' + position).addClass('active');
     
   });
+
+  
   
 });
+
+/*function switch_color() {
+var test = 0;
+if (test = 0){
+  document.getElementsByClassName("test")[0].style.background = "rgba(29, 230, 29, 0.7)";
+  var test = 1;
+  console.log(test);
+  } else {
+   document.getElementsByClassName("test")[0].style.background = "rgba(29, 230, 29, 0)";
+  var test = 0;
+  console.log(test);
+  }
+  };*/
+  function switch_color1() {
+  document.getElementsByClassName("test")[0].style.display = "flex";
+  }
+   function switch_color2() {
+  document.getElementsByClassName("test")[0].style.display = "none";
+  }
